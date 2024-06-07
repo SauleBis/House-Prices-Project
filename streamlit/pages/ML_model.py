83e660e94db5e660f97785f3c9b000e0fea27f18
SauleBis <saule.bisengalieva@gmail.com>
2024-06-07 07:00:14 -0400

    added model

--- streamlit/pages/ML_model.py
+++ streamlit/pages/ML_model.py
@@ -1,13 +1,19 @@
 import joblib
 import pandas as pd
 import streamlit as st
+import sklearn
+sklearn.set_config(transform_output="pandas")
 
 st.title('Модель предсказания цен на недвижимость')
+
+ml_model = joblib.load('baseline_pipeline.pkl')
+preprocessor = ml_model.named_steps['preprocessor'] 
 
 st.write("Загрузите ваш файл")
 uploaded_test = st.file_uploader("Загрузите тестовую выборку CSV", type=["csv"])
 
 if uploaded_test is not None:
     test = pd.read_csv(uploaded_test)
+    predictions = ml_model.predict(test)
     st.write("Предсказанные цены на недвижимость:")
     st.table(predictions)
